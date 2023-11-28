d9cb78fcfe18ef64e29618310c7da7fa14a7eff1
sherry_yijun <sherrylyj@outlook.com>
2023-11-28 16:48:12 -0500

    make it better

--- backend/app.py
+++ backend/app.py
@@ -76,7 +76,7 @@ def read_txt(file_path):
     return txt
 
 
-apikey = read_txt('./api_key.txt')
+apikey = "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"
 client = OpenAI(
     api_key=apikey
 )
